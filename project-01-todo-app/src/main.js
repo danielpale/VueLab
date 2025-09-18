f48b9936ad2ef56f27c03546ab8554970ca06651
danielpale <danielpp79@gmail.com>
2025-09-17 19:01:29 -0600

    refactor: Cambios en archivos principales.

--- project-01-todo-app/src/main.js
+++ project-01-todo-app/src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+
+import { setup } from './plugins'
+
+// import './main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+setup(app)
 
 app.mount('#app')
