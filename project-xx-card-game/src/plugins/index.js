7160736452658cbb6aa170b8450dccca780a3d5d
danielpale <danielpp79@gmail.com>
2025-12-03 17:16:01 -0600

    refactor: Deletion of project card game (it was move to his own repository)

--- project-xx-card-game/src/plugins/index.js
+++
@@ -1,9 +0,0 @@
-import router from '@/router'
-import { createPinia } from 'pinia'
-import { vuetify } from './vuetify'
-
-export function setup(app) {
-  app.use(createPinia())
-  app.use(router)
-  app.use(vuetify)
-}
